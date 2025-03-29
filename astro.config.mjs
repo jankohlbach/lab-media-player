fb2f68288a6df7424d2ec6c7dfd0474ad790d731
Jan Kohlbach <mail@jankohlbach.dev>
2025-03-29 15:56:38 +0100

    📈 add tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: change to final domain
-  site: 'https://astro-creative-base.vercel.app',
+  site: 'https://lab-media-player.vercel.app',
   integrations: [
     robotsTxt({
       sitemapBaseFileName: 'sitemap-index',
